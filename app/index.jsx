f4592c63251bf7f8d0e1b7cce6a76d7aa0f27d2a
Mohamed habib <39647297+mouhamedhabib@users.noreply.github.com>
2026-08-18 11:43:32 +0100

    Update package configuration and add Home component
    - Changed main entry point in package.json to "expo-router/entry".
    - Added new dependencies: expo-constants, expo-linking, expo-router, react-native-safe-area-context, and react-native-screens.
    - Created a new Home component in app/index.jsx with basic layout and styling.

---
+++ app/index.jsx
@@ -0,0 +1,45 @@
+import { View, Text , StyleSheet , Image } from "react-native";
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: 'https://png.pngtree.com/png-clipart/20190611/original/pngtree-wolf-logo-png-image_2306634.jpg' }} style={styles.image} />
+      <Text style={styles.title}>home</Text>
+      <Text>Welcome to the React Native App!</Text>
+      <View style={styles.card}>
+        <Text>
+          This is a simple React Native application that demonstrates the basic structure and components of a mobile app built using React Native. You can customize this app by adding more features, screens, and styles as needed.
+        </Text>
+      </View>
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginBottom: 20,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  card: {
+    backgroundColor: '#f0f0f0',
+    padding: 10,
+    marginVertical: 5,
+    borderRadius: 5,
+  }
+
+
+})
