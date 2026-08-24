39a60f6fb2281d7e1cdc4913a8012c2869d5997b
Mohamed habib <39647297+mouhamedhabib@users.noreply.github.com>
2026-08-24 20:19:48 +0100

    Refactor authentication flow by adding Login and Register screens, and updating navigation structure

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,30 +1,25 @@
-import { StyleSheet, Text, useColorScheme, View } from 'react-native'
-import React from 'react'
-import { Slot, Stack } from 'expo-router'
-import {Colors} from '../constants/Colors'
-import { StatusBar } from 'expo-status-bar'
+import { Stack } from "expo-router"
+import { Colors } from "../constants/Colors"
+import { useColorScheme } from "react-native"
+import { StatusBar } from "expo-status-bar"
 
-const RootLayout = () => {
+export default function RootLayout() {
   const colorScheme = useColorScheme()
   const theme = Colors[colorScheme] ?? Colors.light
 
   return (
     <>
-      <StatusBar value="auto" style={colorScheme === 'dark' ? 'light' : 'dark'} />
-    <Stack screenOptions={{
-      headerStyle: { backgroundColor: theme.navBackground },
-      headerTintColor: theme.title,
-      headerTitleStyle: { fontWeight: 'bold' },
-    }} >
-      <Stack.Screen name="index" option={{ title: 'Home' }} /> 
-      <Stack.Screen name="about" option={{ title: 'About' }} />
-      <Stack.Screen name="contact" option={{ title: 'Contact' }} />
+      <StatusBar value="auto" />
+      <Stack screenOptions={{
+        headerStyle: { backgroundColor: theme.navBackground },
+        headerTintColor: theme.title,
+      }}>
+        {/* Groups */}
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+
+        {/* Individual Screens */}
+        <Stack.Screen name="index" options={{ title: "Home" }} />
       </Stack>
-      </>
-   
+    </>
   )
 }
-
-export default RootLayout
-
-const styles = StyleSheet.create({})
