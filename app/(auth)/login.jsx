6a59d5c2e46e65ed8fbfcfebf8937c676413ac12
Mohamed habib <39647297+mouhamedhabib@users.noreply.github.com>
2026-08-26 20:58:01 +0100

    Implement authentication and dashboard layout with themed buttons and navigation

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -1,11 +1,19 @@
-import { StyleSheet } from 'react-native'
-import { Link } from 'expo-router'
+import { StyleSheet, Text } from 'react-native'
+import { Link, router } from 'expo-router'
+import { Colors } from '../../constants/Colors'
 
 import ThemedView from "../../components/thmedView"
 import ThemedText from '../../components/ThemedText'
 import Spacer from '../../components/Spacer'
+import ThemedButton from '../../components/ThemedButton'
 
 const Login = () => {
+
+  const handleSubmit = async () => {
+    console.log('login form submitted')
+    router.replace('/')
+  }
+
   return (
     <ThemedView style={styles.container}>
       
@@ -13,6 +21,10 @@ const Login = () => {
       <ThemedText title={true} style={styles.title}>
         Login to Your Account
       </ThemedText>
+
+      <ThemedButton onPress={handleSubmit}>
+        <Text style={{ color: '#f2f2f2' }}>Login</Text>
+      </ThemedButton>
 
       <Spacer height={100} />
       <Link href="/register" replace>
@@ -31,10 +43,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+    alignItems: "center"
   },
   title: {
     textAlign: "center",
     fontSize: 18,
     marginBottom: 30
   },
+  btn: {
+    backgroundColor: Colors.primary,
+    padding: 15,
+    borderRadius: 5,
+  },
+  pressed: {
+    opacity: 0.8
+  }
 })
--- app/index.jsx
+++ app/index.jsx
@@ -5,6 +5,7 @@ import ThemedView from "../components/thmedView"
 import ThemedText from "../components/ThemedText"
 import ThemedLogo from "../components/ThemedLogo"
 import Spacer from "../components/Spacer"
+import { Colors } from '../constants/Colors'
 
 const Home = () => {
   return (
@@ -25,7 +26,11 @@ const Home = () => {
       <Link href="/register" style={styles.link}>
         <ThemedText>Register</ThemedText>
       </Link>
-      
+
+      <Link href="/profile" style={styles.link}>
+        <ThemedText>Profile</ThemedText>
+      </Link>
+
     </ThemedView>
   )
 }
@@ -48,5 +53,5 @@ const styles = StyleSheet.create({
   link: {
     marginVertical: 10,
     borderBottomWidth: 1
-  }
+  },
 })
